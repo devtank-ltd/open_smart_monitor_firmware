23cf2d4a4bfdf38764f300df655a26c293627a6a
Sam Wilson <sam.wilson@devtank.co.uk>
2019-08-22 16:53:42 +0100

    Initial work on the particulate matter sensor.

--- main/entrypoint.c
+++ main/entrypoint.c
@@ -15,11 +15,7 @@
 #include "driver/uart.h"
 #include "esp32/rom/uart.h"
 
-// The SDK provides the symbols UART_NUM_0, UART_NUM_1 and UART_NUM_2. Better 
-// not use those. Use the symbols defined below instead; you know, just in case
-// we want to juggle things around a bit which we might want to do.
-#define LORAUART  UART_NUM_2
-#define HPMUART   UART_NUM_1
+#include "pinmap.h"
 
 unsigned long __stack_chk_guard;
 void __stack_chk_guard_setup(void)
@@ -36,7 +32,6 @@ void notification(const char * n) {
     fflush(stdout);
 }
 
-
 void lora_uart_setup() {
     /* UART setup */
     uart_config_t lora = {
@@ -45,12 +40,11 @@ void lora_uart_setup() {
         /* UART parity mode */          .parity = UART_PARITY_DISABLE,
         /* UART stop bits */            .stop_bits = UART_STOP_BITS_1,
         /* UART HW flow control mode */ .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-//      /* UART HW RTS threshold */     .rx_flow_ctrl_thresh = 122,
     };
     ESP_ERROR_CHECK(uart_param_config(LORAUART, &lora));
 
     //esp_err_t uart_set_pin(uart_port_t uart_num, int tx_io_num, int rx_io_num, int rts_io_num, int cts_io_num);
-    if(uart_set_pin(LORAUART, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE) == ESP_FAIL)
+    if(uart_set_pin(LORAUART, LORAUART_TX, LORAUART_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE) == ESP_FAIL)
         printf("Error in uart_set_pin!\n");
     notification("UART PINS CONFIGURED");
 
@@ -71,12 +65,11 @@ void hpm_uart_setup() {
         /* UART parity mode */          .parity = UART_PARITY_DISABLE,
         /* UART stop bits */            .stop_bits = UART_STOP_BITS_1,
         /* UART HW flow control mode */ .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        /* UART HW RTS threshold */     .rx_flow_ctrl_thresh = 122,
     };
     ESP_ERROR_CHECK(uart_param_config(HPMUART, &hpm));
 
     //esp_err_t uart_set_pin(uart_port_t uart_num, int tx_io_num, int rx_io_num, int rts_io_num, int cts_io_num);
-    if(uart_set_pin(HPMUART, 9, 10, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE) == ESP_FAIL)
+    if(uart_set_pin(HPMUART, HPMUART_TX, HPMUART_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE) == ESP_FAIL)
         printf("Error in uart_set_pin!\n");
     notification("UART PINS CONFIGURED");
 
@@ -84,12 +77,60 @@ void hpm_uart_setup() {
     ESP_ERROR_CHECK(uart_driver_install(HPMUART, uart_buffer_size, uart_buffer_size, 10, NULL, 0));
 }
 
-int hpm_query() {
+int hpm_query(int * pm25, int * pm10) {
+    // The body of this function implements my understanding of the protocol laid out by table 4 in the datasheet from Honeywell.
+
+    // Send the request for the measurement
     static char hpm_send[4] = {0x68, 0x01, 0x04, 0x93};
     if(uart_tx_chars(HPMUART, hpm_send, 4) != 4) {
         printf("Error querying the HPM module\n");
+        return -1;
     }
-    return 0;
+
+    // One of two possibilities:
+    // The device responds with a positive acknowledgment, which includes the measurement itself, 
+    // OR the device responds with a negative ACK which doesn't.
+    uint8_t data[128];
+    int length;
+    uart_get_buffered_data_len(LORAUART, (size_t*)&length);
+    length = uart_read_bytes(LORAUART, data, length, 2000);
+
+    // Check if we got a negative ACK and bail out if so.
+    // The negative ACK is two bytes long and reads: 0x96 0x96
+    if(length == 2) {
+       if(data[0] == 0x96 && data[1] == 0x96) {
+            printf("Negative ACK from HPM module");
+            return -1;
+        } else {
+            goto unknown_response;
+        }
+    }
+
+    if(length == 8) {
+        uint8_t head = data[0];
+        uint8_t len = data[1];
+        uint8_t cmd = data[2];
+        uint8_t df1 = data[3];
+        uint8_t df2 = data[4];
+        uint8_t df3 = data[5];
+        uint8_t df4 = data[6];
+        uint8_t checksum = data[7];
+
+        if(checksum != (65536 - (head + len + cmd + df1 + df2 + df3 + df4) % 256)) {
+            printf("HPM checksum error ");
+            goto unknown_response;
+        }
+
+        * pm25 = df1 * 256 + df2;
+        * pm10 = df3 * 256 + df4;
+    }
+
+unknown_response:
+    printf("Unknown response from HPM module ");
+    for(int i = 0; i < length; i++) printf("0x%1x ", data[i]);
+    if(length == 0) printf(" - is it even connected?");
+    printf("\n");
+    return -1;
 }
 
 void app_main(void)
@@ -100,10 +141,14 @@ void app_main(void)
     lora_uart_setup();
 
     notification("CONFIGURING UART1 for HPM MODULE");
-    //hpm_uart_setup();
+    hpm_uart_setup();
 
     notification("QUERYING PARTICULATE MATTER SENSOR");
-    //printf("dust: %d", hpm_query());
+    int pm25, pm10;
+
+    if(!hpm_query(&pm25, &pm10)) {
+        printf("PM2.5 = %d, PM10 = %d\n", pm25, pm10);
+    }
 
     /* Print chip information */
     for (int i = 9; i >= 0; i--) {
