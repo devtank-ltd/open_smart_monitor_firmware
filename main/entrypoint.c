44056ce188fc93b593c68d105a5e134a829a24c0
Sam Wilson <sam.wilson@devtank.co.uk>
2019-09-11 16:39:25 +0100

    Improve debugging output

--- main/entrypoint.c
+++ main/entrypoint.c
@@ -37,19 +37,16 @@ void lora_uart_setup() {
     };
     ESP_ERROR_CHECK(uart_param_config(LORA_UART, &lora));
 
-    //esp_err_t uart_set_pin(uart_port_t uart_num, int tx_io_num, int rx_io_num, int rts_io_num, int cts_io_num);
-    if(uart_set_pin(LORA_UART, LORA_UART_TX, LORA_UART_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE) == ESP_FAIL)
-        printf("Error in uart_set_pin!\n");
-    notification("UART PINS CONFIGURED");
+//    esp_err_t err = uart_set_pin(LORA_UART, LORA_UART_TX, LORA_UART_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    esp_err_t err = uart_set_pin(LORA_UART, 17, 18, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    if(err != ESP_OK) {
+        printf("Trouble %s setting the pins up!\n", esp_err_to_name(err));
+        while(1);
+    }
 
-    const int uart_buffer_size = (1024 * 2);
+    const int uart_buffer_size = 1024;
     ESP_ERROR_CHECK(uart_driver_install(LORA_UART, uart_buffer_size, uart_buffer_size, 10, NULL, 0));
 
-    notification("PRINTING TO LORA");
-//    if(uart_tx_chars(LORA_UART, "Hello, world! \n", 15) != 15)
-//        printf("Error printing a string to the UART");
-//    ESP_ERROR_CHECK(uart_wait_tx_done(LORA_UART, 100));
-    mqtt_sn_send("f5", "fish5 is a live", 0);
 }
 
 void i2c_setup() {
@@ -72,45 +69,49 @@ void i2c_setup() {
     return true;
 }
 
+void mqtt_sn_notify(const char * ident, const char * msg) {
+    printf("mqtt_notify topic %s\t\"%s\"\n", ident, msg);
+    mqtt_sn_send(ident, msg, 0);
+}
+
 void app_main(void)
 {
     notification("ENTRYPOINT REACHED");
 
-//    notification("CONFIGURING UART2 for LORA");
-//    lora_uart_setup();
-
-    notification("CONFIGURING UART1 for HPM MODULE");
+    notification("CONFIGURING UARTS AND I2C");
+    lora_uart_setup();
     hpm_uart_setup();
-
-    notification("INSTALLING I2C DRIVER");
     i2c_setup();
 
     for(;;) {
 
-//        notification("QUERYING PARTICULATE MATTER SENSOR");
-        uint16_t pm25, pm10;
+        mqtt_sn_send("f5", "fish5 is a live", 0);
 
+        notification("Restarting now.\n");
+        uint16_t pm25, pm10;
+        char msg[512];
+        
         if(!hpm_query(&pm25, &pm10)) {
             printf("PM2.5 = %d\nPM10 = %d\n", pm25, pm10);
         }
+        notification("finished querying hpm.\n");
 
-//        notification("QUERYING TEMP & HUMIDITY SENSOR");
         float relative_humidity;
         float temp_celsius;
         hdc_query(&temp_celsius, &relative_humidity);
         printf("Temperature: %f degrees celcius\n", temp_celsius);
         printf("%f%% relative humidity\n", relative_humidity);
 
-//        notification("AMBIENT LIGHT SENSOR");
         uint16_t ch0 = 0;
         uint16_t ch1 = 0;
-        tsl_query(&ch0, &ch1);
+//        tsl_query(&ch0, &ch1);
         printf("CH0 (visible light) = 0x%04x\nCH1 (infrared) = 0x%04x\n", ch0, ch1);
 
-
+        sprintf(&msg, "PM2.5 = %u", (uint)pm25);
+        mqtt_sn_notify("f5", msg);
+        
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    printf("Restarting now.\n");
 
     fflush(stdout);
     esp_restart();
