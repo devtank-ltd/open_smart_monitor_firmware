adbb9d7f94b2ebc58b694ccb8b17c27731d6d958
Sam Wilson <sam.wilson@devtank.co.uk>
2019-09-13 15:14:36 +0100

    Make sure not to overrun the buffer writing to LoRa UART

--- main/mqtt-sn.c
+++ main/mqtt-sn.c
@@ -13,7 +13,32 @@
 #define MQTT_SN_FLAG_RETAIN           (0x1 << 4)
 #define MQTT_SN_TOPIC_TYPE_SHORT      (0x02)
 
-#define SFNODE      '9'
+#define SFNODE '9'
+
+void sendthebytes(const char * str, unsigned int len) {
+    while(len)
+    {
+        esp_err_t err = uart_wait_tx_done(LORA_UART, 100);
+        if(err == ESP_OK)
+        {
+            int sent = uart_tx_chars(LORA_UART, str, len);
+            if (sent > 0)
+            {
+                len -= sent;
+                str += sent;
+//                for(int i = 0; i <= sent; i++) printf("\n - 0x%02x", str[i]);
+            }
+            else if (sent < 0)
+            {
+                printf("Error writing to UART\n");
+            }
+        } else {
+            printf("Trouble %s writing to the LoRa UART.\n", esp_err_to_name(err));
+            vTaskDelay(1);
+        }
+    }
+}
+
 
 // This funcion was shamelessly stolen from 
 // https://github.com/njh/DangerMinusOne/blob/master/DangerMinusOne.ino
@@ -42,13 +67,11 @@ void mqtt_sn_send(const char topic[2], const char * message, bool retain)
     header[5] = 0x00;  // Message ID High
     header[6] = 0x00;  // message ID Low;
 
-    if(uart_wait_tx_done(LORA_UART, 100) == ESP_ERR_TIMEOUT) printf("Timeout while waiting1 for the MQTT packet to leave\n");
-    uart_tx_chars(LORA_UART, header, 7);
-    if(uart_wait_tx_done(LORA_UART, 100) == ESP_ERR_TIMEOUT) printf("Timeout while waiting2 for the MQTT packet to leave\n");
-    uart_tx_chars(LORA_UART, message, len);
-    if(uart_wait_tx_done(LORA_UART, 100) == ESP_ERR_TIMEOUT) printf("Timeout while waiting3 for the MQTT packet to leave\n");
+    sendthebytes(header, 7);
+    sendthebytes(message, len);
 
-    printf("sent msg %s\n", message);
+    printf("sent msg %s, length %d\n", message, sizeof(header) + len);
+    vTaskDelay(200);
 }
 
 void mqtt_update(const char ident, const char * msg) {
