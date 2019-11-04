9fff820d151e4eb1f15ffe9986ea64c427547529
Sam Wilson <sam.wilson@devtank.co.uk>
2019-11-04 15:57:43 +0000

    Switch to using freemodbus instead of homegrown rubbish

--- main/countis_e53.c
+++ main/countis_e53.c
@@ -12,6 +12,8 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "driver/uart.h"
+#include "esp_modbus_master.h"
+
 #define UART_NUM UART_NUM_1
 #define HPM_UART_TX 17
 #define HPM_UART_RX 16
@@ -20,129 +22,93 @@
 #define E53_ADDR 5
 #define HOURMETER 50512
 
-uint16_t modbus_crc(uint8_t * buf, int length) {
-    static const uint16_t crctable[] = {
-    0X0000, 0XC0C1, 0XC181, 0X0140, 0XC301, 0X03C0, 0X0280, 0XC241,
-    0XC601, 0X06C0, 0X0780, 0XC741, 0X0500, 0XC5C1, 0XC481, 0X0440,
-    0XCC01, 0X0CC0, 0X0D80, 0XCD41, 0X0F00, 0XCFC1, 0XCE81, 0X0E40,
-    0X0A00, 0XCAC1, 0XCB81, 0X0B40, 0XC901, 0X09C0, 0X0880, 0XC841,
-    0XD801, 0X18C0, 0X1980, 0XD941, 0X1B00, 0XDBC1, 0XDA81, 0X1A40,
-    0X1E00, 0XDEC1, 0XDF81, 0X1F40, 0XDD01, 0X1DC0, 0X1C80, 0XDC41,
-    0X1400, 0XD4C1, 0XD581, 0X1540, 0XD701, 0X17C0, 0X1680, 0XD641,
-    0XD201, 0X12C0, 0X1380, 0XD341, 0X1100, 0XD1C1, 0XD081, 0X1040,
-    0XF001, 0X30C0, 0X3180, 0XF141, 0X3300, 0XF3C1, 0XF281, 0X3240,
-    0X3600, 0XF6C1, 0XF781, 0X3740, 0XF501, 0X35C0, 0X3480, 0XF441,
-    0X3C00, 0XFCC1, 0XFD81, 0X3D40, 0XFF01, 0X3FC0, 0X3E80, 0XFE41,
-    0XFA01, 0X3AC0, 0X3B80, 0XFB41, 0X3900, 0XF9C1, 0XF881, 0X3840,
-    0X2800, 0XE8C1, 0XE981, 0X2940, 0XEB01, 0X2BC0, 0X2A80, 0XEA41,
-    0XEE01, 0X2EC0, 0X2F80, 0XEF41, 0X2D00, 0XEDC1, 0XEC81, 0X2C40,
-    0XE401, 0X24C0, 0X2580, 0XE541, 0X2700, 0XE7C1, 0XE681, 0X2640,
-    0X2200, 0XE2C1, 0XE381, 0X2340, 0XE101, 0X21C0, 0X2080, 0XE041,
-    0XA001, 0X60C0, 0X6180, 0XA141, 0X6300, 0XA3C1, 0XA281, 0X6240,
-    0X6600, 0XA6C1, 0XA781, 0X6740, 0XA501, 0X65C0, 0X6480, 0XA441,
-    0X6C00, 0XACC1, 0XAD81, 0X6D40, 0XAF01, 0X6FC0, 0X6E80, 0XAE41,
-    0XAA01, 0X6AC0, 0X6B80, 0XAB41, 0X6900, 0XA9C1, 0XA881, 0X6840,
-    0X7800, 0XB8C1, 0XB981, 0X7940, 0XBB01, 0X7BC0, 0X7A80, 0XBA41,
-    0XBE01, 0X7EC0, 0X7F80, 0XBF41, 0X7D00, 0XBDC1, 0XBC81, 0X7C40,
-    0XB401, 0X74C0, 0X7580, 0XB541, 0X7700, 0XB7C1, 0XB681, 0X7640,
-    0X7200, 0XB2C1, 0XB381, 0X7340, 0XB101, 0X71C0, 0X7080, 0XB041,
-    0X5000, 0X90C1, 0X9181, 0X5140, 0X9301, 0X53C0, 0X5280, 0X9241,
-    0X9601, 0X56C0, 0X5780, 0X9741, 0X5500, 0X95C1, 0X9481, 0X5440,
-    0X9C01, 0X5CC0, 0X5D80, 0X9D41, 0X5F00, 0X9FC1, 0X9E81, 0X5E40,
-    0X5A00, 0X9AC1, 0X9B81, 0X5B40, 0X9901, 0X59C0, 0X5880, 0X9841,
-    0X8801, 0X48C0, 0X4980, 0X8941, 0X4B00, 0X8BC1, 0X8A81, 0X4A40,
-    0X4E00, 0X8EC1, 0X8F81, 0X4F40, 0X8D01, 0X4DC0, 0X4C80, 0X8C41,
-    0X4400, 0X84C1, 0X8581, 0X4540, 0X8701, 0X47C0, 0X4680, 0X8641,
-    0X8201, 0X42C0, 0X4380, 0X8341, 0X4100, 0X81C1, 0X8081, 0X4040 };
+#include "esp_log.h"
+#include <string.h> // needed for memset down there
 
-    unsigned char nTemp;
-    uint16_t crc = 0xffff;
-    while(length--) {
-        nTemp = *buf++ ^ crc;
-        crc >>= 8;
-        crc ^= crctable[nTemp];
-    }   
-    return crc;
+#define TAG "MODBUS"
+#define SENSE_MB_CHECK(a, ret_val, str, ...) \
+    if (!(a)) { \
+        ESP_LOGE(TAG, "%s(%u): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+        return (ret_val); \
+    }
+#define OPTS(min_val, max_val, step_val) { .opt1 = min_val, .opt2 = max_val, .opt3 = step_val }
+#define NOOPTS { .opt1 = 0, .opt2 = 0, .opt3 = 0 }
+
+const mb_parameter_descriptor_t countis_e53[] = { \
+ // { Cid, Param Name,                     Units,                           SlaveAddr,Modbus Reg Type,  Start,Size,Offs, Data Type,DataSize,ParamOptions,Access Mode}
+    { 0,   (const char *)"Hour meter",     (const char *)"watt/hours /100", E53_ADDR, MB_PARAM_HOLDING, 50512,   4,   0, PARAM_TYPE_U32, 4, NOOPTS, PAR_PERMS_READ },
+    { 1,   (const char *)"Apparent power", (const char *)"VA / 0.1",        E53_ADDR, MB_PARAM_HOLDING, 50536,   4,   0, PARAM_TYPE_U32, 4, OPTS(0,0,0), PAR_PERMS_READ },
+    { 2,   (const char *)"Hour meter",     (const char *)"watt/hours /100", E53_ADDR, MB_PARAM_HOLDING, 50592,   4,   0, PARAM_TYPE_U32, 4, OPTS(0,0,0), PAR_PERMS_READ },
+    { 3,   (const char *)"Network type",   (const char *)"Network type",    E53_ADDR, MB_PARAM_HOLDING, 40448,   1,   0, PARAM_TYPE_U8,  1, OPTS(0,0,0), PAR_PERMS_READ },
+};
+
+const uint16_t num_device_parameters = (sizeof(countis_e53)/sizeof(countis_e53[0]));
+
+// Read characteristic value from Modbus parameter according to description table
+esp_err_t sense_modbus_read_value(uint16_t cid, void *value)
+{
+    uint8_t type = 0;
+    mb_parameter_descriptor_t cid_info = countis_e53[cid];
+
+    // Send Modbus request to read cid correspond registers
+    esp_err_t error = mbc_master_get_parameter(cid, (char*)cid_info.param_key, value, &type);
+    SENSE_MB_CHECK((type <= PARAM_TYPE_ASCII), ESP_ERR_NOT_SUPPORTED, "returned data type is not supported (%u)", type);
+    return error;
 }
 
-uint16_t read_holding_register(uint16_t reg) {
-    unsigned char msg[8];
-    uint16_t crc;
+esp_err_t init_smart_meter() {
 
-    msg[0] = E53_ADDR;
-    msg[1] = 3;
-    msg[2] = reg >> 8 & 0xff;
-    msg[3] = reg & 0xff;
-    msg[4] = 0;
-    msg[5] = 1;
+    mb_communication_info_t comm = { 
+            .port = UART_NUM_1,
+            .mode = MB_MODE_RTU,
+            .baudrate = 9600,
+            .parity = UART_PARITY_DISABLE
+    };  
+    void* master_handler = NULL;
 
-    crc = modbus_crc(msg, 6);
-    msg[6] = crc & 0xff;
-    msg[7] = crc >> 8 & 0xff;
+    esp_err_t err = mbc_master_init(MB_PORT_SERIAL_MASTER, &master_handler);
+    SENSE_MB_CHECK((master_handler != NULL), ESP_ERR_INVALID_STATE,
+                                "mb controller initialization fail.");
+    SENSE_MB_CHECK((err == ESP_OK), ESP_ERR_INVALID_STATE,
+                            "mb controller initialization fail, returns(0x%x).",
+                            (uint32_t)err);
+    err = mbc_master_setup((void*)&comm);
+    SENSE_MB_CHECK((err == ESP_OK), ESP_ERR_INVALID_STATE,
+                            "mb controller setup fail, returns(0x%x).",
+                            (uint32_t)err);
+    err = mbc_master_start();
+    SENSE_MB_CHECK((err == ESP_OK), ESP_ERR_INVALID_STATE,
+                            "mb controller start fail, returns(0x%x).",
+                            (uint32_t)err);
+    vTaskDelay(5);
+    err = mbc_master_set_descriptor(&countis_e53[0], num_device_parameters);
+    SENSE_MB_CHECK((err == ESP_OK), ESP_ERR_INVALID_STATE,
+                                "mb controller set descriptor fail, returns(0x%x).",
+                                (uint32_t)err);
 
-    if(uart_tx_chars(UART_NUM, (char *)msg, 8) != 8) {
-        printf("Error querying the modbus slave\n");
-        return 0;
-    }
-
-    uart_flush(UART_NUM);
-    printf("Sent modbus command, length 8, as follows:\n");
-    for(int i = 0; i < 8; i++) printf("0x%02x ", msg[i]);
-    printf("\n");
-
-    int response_length;
-
-    if((response_length = uart_read_bytes(UART_NUM, msg, 5, 200)) != 5) {
-        printf("received %d bytes but expected 8 error\n", response_length);
-        return 0;
-    }
-
-    // A modbus response begins with the slave's address.
-    if(msg[0] != E53_ADDR) goto unknown_response;
-    
-    // The second byte ought to be the name of the function again.
-    if(msg[1] != 3) goto unknown_response;
-
-    // The next byte ought to be the length of the response (hard coded to 4 bytes >:-O)
-    if(msg[2] != 4) goto unknown_response;
-    
-
-    // TODO: verify that the CRC is right.
-
-
-    // todo: return the value
-    printf("Got correct response from modbus slave, length %d, as follows:\n", response_length);
-    for(int i = 0; i < response_length; i++) printf("0x%02x ", msg[i]);
-
-    return 0;
-
-unknown_response:
-    printf("Got unknown response from modbus slave, length %d, as follows:\n", response_length);
-    for(int i = 0; i < response_length; i++) printf("0x%02x ", msg[i]);
-
-    return 0;
-}
-
-void init_smart_meter() {
-    uart_config_t hpm = {
-        /* UART baud rate */            .baud_rate = 9600,
-        /* UART byte size */            .data_bits = UART_DATA_8_BITS,
-        /* UART parity mode */          .parity = UART_PARITY_DISABLE,
-        /* UART stop bits */            .stop_bits = UART_STOP_BITS_1,
-        /* UART HW flow control mode */ .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-    };
-    ESP_ERROR_CHECK(uart_param_config(UART_NUM, &hpm));
 
     if(uart_set_pin(UART_NUM, HPM_UART_TX, HPM_UART_RX, RS485_DE, UART_PIN_NO_CHANGE) == ESP_FAIL)
         printf("Error in uart_set_pin!\n");
-    const int uart_buffer_size = 2 * 1024;
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM, uart_buffer_size, uart_buffer_size, 0, NULL, 0));
     uart_set_mode(UART_NUM, UART_MODE_RS485_HALF_DUPLEX);
 
+    return err;
 }
 
 void query_countis()
 {
-    uint16_t hourmeter = read_holding_register(HOURMETER);
-    printf("hourmeter = %d\n", hourmeter);
+    uint32_t hourmeter = 0;
+    uint32_t apparentpower = 0;
+    float fhourmeter = 0.0;
+    uint8_t networktype = 0;
+
+    sense_modbus_read_value(0, &hourmeter);
+    sense_modbus_read_value(1, &apparentpower);
+    sense_modbus_read_value(2, &fhourmeter);
+    sense_modbus_read_value(3, &networktype);
+
+    const char * ntnames[] = { "1bl", "2bl", "3bl", "3nbl", "4bl", "4nbl" };
+
+    printf("network type: %s\n", ntnames[networktype]);
+
+    printf("hourmeter = i%u f%f\napparent_power = %u\n", hourmeter, fhourmeter, apparentpower);
 }
