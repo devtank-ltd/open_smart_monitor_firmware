d0c15f6240034a74e12a239f04b3d26cb70571f4
Sam Wilson <sam.wilson@devtank.co.uk>
2019-11-20 15:33:40 +0000

    Debounce

--- main/volume.c
+++ main/volume.c
@@ -4,16 +4,36 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
+// 1000 microseconds is 1 millisecond.
+#define DEBOUNCE_WAIT 1000
+
 static int litres = 0;
 static int cnt2 = 0;
 static int cnt3 = 0;
 
 static void IRAM_ATTR isr_water(void * arg) {
+    // This pulse input is the only one I've actually made an effort to debounce, since I don't know exactly the characteristics of the other two.
     static int lvl;
-    if(lvl != gpio_get_level(FLOWRATE_GPIO)) {
-        lvl = !lvl;
-        litres += 10;
-    }
+    volatile static int64_t previous_edge = 0;
+
+    // esp_timer_get_time returns in microseconds, the time since startup.
+    // As that's an int64_t there is a slight risk of missing one pulse every 292471 years when this timer wraps
+    volatile int64_t now = esp_timer_get_time();
+
+    // If the transitions are too close together they should not be counted
+    if((now <= previous_edge + DEBOUNCE_WAIT)) return;
+
+    // If the current level is the same as the previous one, then the transition should not be counted
+    // (This eliminates peaks during the high-to-low transition and troughs during the low-to-high transition
+    int nlvl = gpio_get_level(FLOWRATE_GPIO);
+    if(lvl == nlvl) return;
+
+    // We are counting this edge as being a genuine transition.
+    previous_edge = now;
+    lvl = nlvl;
+    
+    // If this edge was a falling one, then the count has gone up by one.
+    if(lvl) litres++;
 }
 
 static void IRAM_ATTR isr_p1(void * arg) {
@@ -50,5 +70,5 @@ void volume_setup() {
 
 void qry_volume() {
     printf("volume = %d\n", litres);
-    printf("pulsecounts: %d, %d.\n", cnt2, cnt3);
+//    printf("pulsecounts: %d, %d.\n", cnt2, cnt3);
 }
