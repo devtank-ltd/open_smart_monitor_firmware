bb02c2c69e3a82b3c9fc17a762ada89ac6552ad5
Sam Wilson <sam.wilson@devtank.co.uk>
2019-11-21 13:47:25 +0000

    Can query both inputs, count and frequency

--- main/volume.c
+++ main/volume.c
@@ -1,5 +1,6 @@
 #include "driver/gpio.h"
 #include "pinmap.h"
+#include "mqtt-sn.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
@@ -7,8 +8,11 @@
 // 1000 microseconds is 1 millisecond.
 #define DEBOUNCE_WAIT 1000
 
-static int count1 = 0;
-static int count2 = 0;
+static volatile int count1 = 0;
+static volatile int count2 = 0;
+static volatile int freq1 = 0;
+static volatile int freq2 = 0;
+
 
 static inline int debounce(int64_t * old_time, int * old_level, int new_level) {
     // esp_timer_get_time returns in microseconds, the time since startup.
@@ -16,7 +20,7 @@ static inline int debounce(int64_t * old_time, int * old_level, int new_level) {
     volatile int64_t now = esp_timer_get_time();
 
     // If the transitions are too close together they should not be counted
-    if((now <= previous_edge + DEBOUNCE_WAIT)) return 0;
+    if((now <= *old_time + DEBOUNCE_WAIT)) return 0;
 
     // If the current level is the same as the previous one, then the transition should not be counted
     // (This eliminates peaks during the high-to-low transition and troughs during the low-to-high transition
@@ -31,21 +35,26 @@ static inline int debounce(int64_t * old_time, int * old_level, int new_level) {
 
 static void IRAM_ATTR isr_p2(void * arg) {
     static int level;
-    volatile static int64_t previous_edge = 0;
+    static int64_t previous_edge = 0;
     int new_level = gpio_get_level(PULSE_IN_2);
     if(debounce(&previous_edge, &level, new_level) && new_level) count2++;
 }
 
 static void IRAM_ATTR isr_p1(void * arg) {
     static int level;
-    volatile static int64_t previous_edge = 0;
+    static int64_t previous_edge = 0;
     int new_level = gpio_get_level(PULSE_IN_1);
     if(debounce(&previous_edge, &level, new_level) && new_level) count1++;
 }
 
-void freq_report(void * arg) {
+void freq_compute(void * arg) {
+    // Have this function run every second, and it will compute the
+    // frequency of both pulses. In hertz.
+    static int old_count1 = 0;
     static int old_count2 = 0;
-    printf("%d Hz\n", count2 - old_count2);
+    freq1 = count1 - old_count1;
+    freq2 = count2 - old_count2;
+    old_count1 = count1;
     old_count2 = count2;
 }
 
@@ -58,7 +67,7 @@ void volume_setup() {
     io_conf.pull_up_en = 1;
 
     const esp_timer_create_args_t periodic_timer_args = {
-        .callback = &freq_report,
+        .callback = &freq_compute,
         .name = "freq_report"
     };
 
@@ -73,7 +82,6 @@ void volume_setup() {
     ESP_ERROR_CHECK(gpio_isr_handler_add(PULSE_IN_2, isr_p2, (void*) PULSE_IN_2));
 }
 
-void qry_volume() {
-    printf("volume = %d\n", count2);
-//    printf("pulsecounts: %d, %d.\n", cnt2, cnt3);
+void qry_pulsecount(const char * key, int multiplier, int which) {
+    mqtt_announce_int(key, (which ? count1 : count2) * multiplier);
 }
--- main/volume.h
+++ main/volume.h
@@ -1,5 +1,7 @@
 #ifndef __VOLUME_H
 void volume_setup();
-void qry_volume();
+void qry_pulsecount(const char * key, int multiplier, int which);
+void qry_frequency(const char * key, int which);
+
 #define __VOLUME_H
 #endif
