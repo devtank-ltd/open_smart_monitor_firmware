f87fdf04c93d81badc14a85f9cef5d1c05d12aa2
Sam Wilson <sam.wilson@devtank.co.uk>
2020-03-31 12:45:44 +0100

    Better colours

--- main/logging.h
+++ main/logging.h
@@ -1,4 +1,4 @@
-#define INFO_PRINTF(f_, ...)  puts("\033[32;42m"); printf((f_), ##__VA_ARGS__)
-#define ERROR_PRINTF(f_, ...) puts("\033[31;40m"); printf((f_), ##__VA_ARGS__)
-#define DEBUG_PRINTF(f_, ...) puts("\033[36;40m"); printf((f_), ##__VA_ARGS__)
+#define INFO_PRINTF(f_, ...)  printf(("\033[37m" f_ "\n"), ##__VA_ARGS__)
+#define ERROR_PRINTF(f_, ...) printf(("\033[31m" f_ "\n"), ##__VA_ARGS__)
+#define DEBUG_PRINTF(f_, ...) printf(("\033[36m" f_ "\n"), ##__VA_ARGS__)
 
