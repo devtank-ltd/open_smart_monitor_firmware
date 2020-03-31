ba1493e82fa189a11fe468944c7ff2e5429add11
Sam Wilson <sam.wilson@devtank.co.uk>
2020-03-31 12:42:46 +0100

    Syntax error in macro expansion

--- main/mac.c
+++ main/mac.c
@@ -12,7 +12,8 @@ void getmac() {
     esp_err_t err;
 
     err = esp_wifi_init(&cfg);
-    ERROR_PRINTF(esp_err_to_name(err));
+    char * error = esp_err_to_name(err);
+    ERROR_PRINTF(error);
     ESP_ERROR_CHECK(esp_wifi_get_mac(WIFI_MODE_STA, mac));
     snprintf(mac_addr, 13, "%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     INFO_PRINTF("My MAC address is %s.", mac_addr);
