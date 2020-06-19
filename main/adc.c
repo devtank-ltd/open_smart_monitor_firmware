83914d24e1f763538e2a6a8f50d59b109aae740c
Sam Wilson <sam.wilson@devtank.co.uk>
2020-06-19 11:56:43 +0100

    breakout

--- main/adc.c
+++ main/adc.c
@@ -70,18 +70,34 @@ void battery_query() {
     mqtt_announce_int("BATMON", v);
 }
 
+double voltagecalc(int adc_count){
+    return adc_count/2048.0 * 3.18;
+}
+
+double dbcalc(int adc_count) {
+    double voltage = adc_count/2048.0 * 3.18;
+    double db = (20*log10(voltage/8.9125*.001))-33.44+94;
+    return db;
+}
+
 void sound_query() {
-    unsigned long vrms = 0;
+    for(int i = 0; i< 14; i++) {
+        printf("%u\t%f\t%f\n", i, voltagecalc(i), dbcalc(i));
+    }
+    long double vrms = 0;
     for (unsigned n = 0; n < ADC_AVG_SLOTS; n++) {
-        vrms += adc_values[n][0];
+        vrms += adc_values[n][0]/4096.0 *3.18;
     }
     vrms = sqrt(vrms / ADC_AVG_SLOTS);
 
     // This equation 
-    float db = (20*log(vrms/8.9125*.001))-33.44+94;
+    float db = (20*log10(vrms/8.9125*.001))-33.44+94;
 
     uint16_t old_db = 0;
     uint16_t idb = db;
+    printf("db = %f\n", db);
+    printf("idb = %d\n", (unsigned int)db);
+
     mqtt_delta_announce_int("SOUNDLEVEL", &idb, &old_db, 1);
     mqtt_announce_int("RawADC", adc1_safe_get(SOUND_OUTPUT));
 }
