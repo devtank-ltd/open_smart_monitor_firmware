9a539e5819be39d975f7bc206971b66aa096ad3a
Sam Wilson <sam.wilson@devtank.co.uk>
2020-07-03 16:24:55 +0100

    No real need to log what the status led is doing.

--- main/status_led.c
+++ main/status_led.c
@@ -15,13 +15,11 @@ void status_led_toggle() {
 void status_led_task(void *pvParameters) {
     gpio_set_direction(STATUS_LED, GPIO_MODE_OUTPUT);
     for(;;) {
-        INFO_PRINTF("Status LED " "on");
         // Turn on the LED for a short time
         gpio_set_level(STATUS_LED, 1);
         vTaskDelay(100 / portTICK_PERIOD_MS);
 
         // Turn off the LED for a somewhat longer time
-        INFO_PRINTF("Status LED " "off");
         gpio_set_level(STATUS_LED, 0);
         vTaskDelay(600 / portTICK_PERIOD_MS);
 
