0505d4bd0fea1c6365d1c944a8affd25826d7981
Sam Wilson <sam.wilson@devtank.co.uk>
2020-07-07 09:26:40 +0100

    add necessary headers to measurement.c

--- main/measurements.c
+++ main/measurements.c
@@ -4,14 +4,29 @@
 #include "logging.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "mqtt-sn.h"
+#include "commit.h"
+#include "hpm.h"
+#include "hdc.h"
+#include "adc.h"
+#include "tsl.h"
+#include "socomec.h"
+#include "volume.h"
 
 void measurements_task(void *pvParameters) {
+    hpm_setup();
+    adc_setup();
+    tsl_setup();
+    volume_setup();
+    smart_meter_setup();
+
+
     for(;;) {
         // These need to be announced very rarely.
         mqtt_announce_str("sku", "ENV-01");
         mqtt_announce_str("fw", GIT_COMMIT);
 
-        for(int i = 0; i < 2; i++) 
+        for(int i = 0; i < 2; i++) {
             for(int j = 0; j < 6000; j++) {
                 // These need to be averaged over ten minutes.
                 hpm_query();   // smog sensor
