ee089da71ed4d7e701666ab9052e50dde45930fe
Sam Wilson <sam.wilson@devtank.co.uk>
2020-09-28 10:50:32 +0100

    Get rid of unused bits and bobs

--- main/config.c
+++ main/config.c
@@ -14,21 +14,6 @@ static inline int is_whitespace(uint8_t c) {
     return c == '\t' || c == ' ' || c == '\n';
 }
 
-static inline int copy_along(uint8_t * dst, uint8_t * src, int len) {
-    size_t ret = 0;
-    while(len && !is_whitespace(*src)) {
-        *dst++ = *src++;
-        len--;
-        ret++;
-    }
-    return ret;
-}
-
-static inline int separate(uint8_t * str, int cur) {
-    while(is_whitespace(str[cur])) cur++;
-    return cur;
-}
-
 static inline char * getfield(char * f) {
     while(!is_whitespace(f[0])) f++;
     f[0] = '\0';
@@ -63,36 +48,3 @@ const char* get_config(const char * key) {
     return value;
 }
 
-void configure() {
-    char message[BUFLEN];
-    size_t length = 0;
-    char * mac = NULL;
-    char * key = NULL;
-    char * val = NULL;
-    uart_get_buffered_data_len(LORA_UART, &length);
-    if(!length) return;
-    length = 0;
-keep_listening:
-    length += uart_read_bytes(LORA_UART, (uint8_t *)message + length, BUFLEN - length, 1);
-    message[length] = '\0';
-    if(length > BUFLEN)             goto procline; // buffer overrun
-    if(message[length - 1] == '\r') goto procline; // carriage return
-    if(message[length - 1] == '\b') length -= 2;   // backspace
-
-    vTaskDelay(1);
-    goto keep_listening;
-
-procline:
-    mac = message;
-    key = getfield(mac);
-    val = getfield(key);
-    getfield(val);
-
-    if(strcmp(mac, mac_addr)) return;
-
-    INFO_PRINTF("mac %zu %s", strlen(mac), mac);
-    INFO_PRINTF("key %zu %s", strlen(key), key);
-    INFO_PRINTF("val %zu %s", strlen(val), val);
-    store_config(key, val);
-    return;
-}
