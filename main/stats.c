5924d1470880843960f6647bb08912696d1410dc
Sam Wilson <sam.wilson@devtank.co.uk>
2021-02-08 09:56:41 +0000

    New task drains queues and computes avg, min, max
    The idea here is that the various drivers and things should be able to just
    stick its measurement in a queue and get on with life. Separately, another
    FreeRTOS task can just keep draining these queues, computing the averages,
    minima and maxima.
    
    This means that:
    - the drivers don't each need a buffer for 1000 samples -- less RAM use
    - the CPU usage will not spike when all avgs etc need computing
    - the update rate is not tied to number of samples
    - Make use of FreeRTOS, more maintainable

--- main/stats.c
+++ main/stats.c
@@ -1,20 +1,82 @@
 #include "stats.h"
-#include "mqtt-sn.h"
-#include "freertos/FreeRTOS.h"
+#include "config.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/FreeRTOS.h"
+#include "mqtt.h"
 
-void stats(int32_t * arr, unsigned int len, mqtt_stats_t * stats) {
-    stats->minimum = arr[0];
-    stats->maximum = arr[0];
-    stats->average = 0;
+#define QUEUESIZE 50
+#define STACKSIZE 10000
 
-    for(int i = 0; i < len; i++) {
-        uint16_t samp = arr[i];
-        if(stats->minimum > samp) stats->minimum = samp;
-        if(stats->maximum < samp) stats->maximum = samp;
-        stats->average += samp;
+typedef volatile struct __ {
+    bool ready;
+    TickType_t updated;
+    TickType_t sent;
+    TickType_t delta;
+    int64_t cumulative;
+    int32_t minimum;
+    int32_t maximum;
+    unsigned int sample_count;
+} stats_t;
+
+
+int last_updated[ARRAY_SIZE(parameter_names)] = {0};
+stats_t stats[ARRAY_SIZE(parameter_names)];
+xQueueHandle queues[ARRAY_SIZE(parameter_names)] = {0};
+
+TaskHandle_t xStatsHandle = NULL;
+StaticTask_t xStatsBuffer;
+StackType_t  xStatsStack[STACKSIZE];
+
+void stats_task(void *pvParameters) {
+    int datum;
+    for(;;) {
+        for(int i = 0; i < ARRAY_SIZE(parameter_names); i++) {
+
+            // Read as many values from the queue as possible
+            while(xQueueReceive(queues[i], &datum, 0) == pdTRUE) {
+			    stats[i].cumulative += datum;
+                if(datum < stats[i].minimum) stats[i].minimum = datum;
+                if(datum > stats[i].maximum) stats[i].maximum = datum;
+                stats[i].sample_count++;
+            }
+
+            /* If enough time has elapsed, then compute the average, and
+             * publish the other data
+             */
+            if(stats[i].updated + stats[i].delta < xTaskGetTickCount()) {
+                stats[i].updated = xTaskGetTickCount();
+                mqtt_enqueue_int(parameter_names[i], "min", stats[i].minimum);
+                mqtt_enqueue_int(parameter_names[i], "max", stats[i].maximum);
+                mqtt_enqueue_int(parameter_names[i], "avg", stats[i].cumulative / stats[i].sample_count);
+            }
+
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
     }
-    stats->average /= len;
-    stats->updated = xTaskGetTickCount();
-    stats->ready=true;
+}
+
+void stats_init() {
+    // Populate the array of queues
+    for(int i = 0; i < ARRAY_SIZE(parameter_names); i++) {
+        queues[i] = xQueueCreate(QUEUESIZE, sizeof(int));
+        stats[i].ready = false;
+        stats[i].updated = 0;
+        stats[i].sent = 0;
+        stats[i].delta = 1 * 60 * 1000 / portTICK_PERIOD_MS;
+        stats[i].cumulative = 0;
+        stats[i].minimum = 0;
+        stats[i].maximum = 0;
+        stats[i].sample_count = 0;
+    }
+
+    xStatsHandle = xTaskCreateStatic(
+                      stats_task, /* Function that implements the task. */
+                      "STATSTASK",     /* Text name for the task. */
+                      STACKSIZE,       /* Number of indexes in the xStack array. */
+                      (void*)1,        /* Parameter passed into the task. */
+                      tskIDLE_PRIORITY,/* Priority at which the task is created. */
+                      xStatsStack,     /* Array to use as the task's stack. */
+                      &xStatsBuffer);  /* Variable to hold the task's data structure. */
+
 }
--- main/stats.h
+++ main/stats.h
@@ -1,3 +1,42 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
 #include "stdint.h"
-#include "mqtt-sn.h"
-void stats(int32_t * arr, unsigned int len, mqtt_stats_t * stats);
+
+#define FOREACH_PARAM(FN) \
+    FN(temperature) \
+    FN(humidity) \
+    FN(sound_level) \
+    FN(pm10) \
+    FN(pm25) \
+    FN(light) \
+    FN(current1) \
+    FN(current2) \
+    FN(current3) \
+    FN(voltage1) \
+    FN(voltage2) \
+    FN(voltage3) \
+    FN(powerfactor) \
+    FN(pfleadlag) \
+    FN(freq1) \
+    FN(freq2) \
+    FN(external_temperature)
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+#define ARRAY_SIZE(_a) (sizeof(_a)/sizeof(_a[0]))
+
+enum parameters {
+    // this macro will expand to temperature, sound_level, humidity, etc.
+    FOREACH_PARAM(GENERATE_ENUM)
+};
+
+static const char * parameter_names[] = {
+    // this macro will expand to "temperature", "sound_level", "humidity", etc.
+    FOREACH_PARAM(GENERATE_STRING)
+};
+
+extern xQueueHandle queues[ARRAY_SIZE(parameter_names)];
+
+void stats_init();
