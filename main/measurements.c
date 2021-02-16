75c0d87112496817f44b7db38bdf5e254c002515
Sam Wilson <sam.wilson@devtank.co.uk>
2021-02-16 22:20:02 +0000

    measurement task revamped

--- main/measurements.c
+++ main/measurements.c
@@ -14,9 +14,11 @@
 #include "volume.h"
 #include "config.h"
 #include "ds18b20.h"
+#include "stats.h"
 
 #define SAMPLE_RATE_MS 1000
-#define TIME_OFFSET (SAMPLE_RATE_MS / portTICK_PERIOD_MS)
+#define TIME_OFFSET pdMS_TO_TICKS(SAMPLE_RATE_MS)
+#define pdTICKS_TO_SECS(xTicks) ((xTicks) / (float)configTICKRATEHZ)
 
 void measurements_task(void *pvParameters) {
     adc_setup();
@@ -26,20 +28,23 @@ void measurements_task(void *pvParameters) {
     ds18b20_init();
     hpm_setup();
 
+    TickType_t next_due[ARRAY_SIZE(parameter_names)];
+    for(int i = 0; i < ARRAY_SIZE(parameter_names); i++)
+        next_due[i] = xTaskGetTickCount();
+
     for(;;) {
         TickType_t before = xTaskGetTickCount();
-    
-        hpm_query();   // smog sensor
-        hdc_query();   // humidity and temperature
-        sound_query(); // sound
-        tsl_query();   // light
-        smart_meter_query();
-        query_pulsecount();
-        battery_query();
-        ds18b20_get_temp();
+        
+        for(int i = 0; i < ARRAY_SIZE(parameter_names); i++) {
+            if(next_due[i] < before) {
+                parameter_getters[i]();
+                TickType_t offs = pdMS_TO_TICKS(get_sample_rate(i) * 1000);
+                next_due[i] = before + offs;
+            }
+        }
 
         TickType_t after = xTaskGetTickCount();
-        TickType_t delay = (before + TIME_OFFSET) - after;
+        TickType_t delay = after - before + pdMS_TO_TICKS(SAMPLE_RATE_MS);
 
         if(before + TIME_OFFSET > after) {
             vTaskDelay(delay);
